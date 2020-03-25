0df4184b876239f898b0d9aabc49a10afd08e93f
olivercastroteran <olivercastroteran@gmail.com>
2020-03-24 23:17:48 -0400

    Logout funtionality added

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -15,3 +15,13 @@ signupForm.addEventListener('submit', e => {
     signupForm.reset();
   });
 });
+
+// Logout
+const logout = document.querySelector('#logout');
+
+logout.addEventListener('click', e => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('User Signed Out');
+  });
+});
