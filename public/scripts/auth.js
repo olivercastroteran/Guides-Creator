cc6912ca1cb552c1f4f1ef973103fce4b3877c94
olivercastroteran <olivercastroteran@gmail.com>
2020-03-25 22:05:20 -0400

    guides creator btn functionality added

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -14,6 +14,27 @@ auth.onAuthStateChanged(user => {
   }
 });
 
+// create new guide
+const createForm = document.querySelector('#create-form');
+createForm.addEventListener('submit', e => {
+  e.preventDefault();
+
+  db.collection('guides')
+    .add({
+      title: createForm['title'].value,
+      content: createForm['content'].value
+    })
+    .then(() => {
+      // close modal and reset form
+      const modal = document.querySelector('#modal-create');
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    })
+    .catch(err => {
+      console.log(err.message);
+    });
+});
+
 // Signup
 const signupForm = document.querySelector('#signup-form');
 
